5d458a138c281650cd8db73fd364db749e30dff5
Cédric Hulin <hulin.cedric@gmail.com>
2021-06-06 08:33:29 +0200

    refactor: Replace by updated version

--- packages/MenusG/MenusG.java
+++ packages/MenusG/MenusG.java
@@ -1,21 +1,23 @@
 //
 // IUT de Nice / Departement informatique / Module APO-Java
-// Annee 2009_2010 - Package AWT
+// Annee 2008_2009 - Package AWT
 //
-// Classe MenusG - Creation d'une barre de menus parametree par un
-//                 dictionnaire de configuration
+// Classe MenusG - Creation d'une barre de menus parametree par des
+//                 dictionnaires de configuration
 //
 // Edition A    : la barre s'accroche automatiquement au cadre support 
 //    + Version 1.0.0 : version limitee aux noms des menus
 //    + Version 1.1.0 : mise en place d'un hamecon sur le frame englobant
 //    + Version 1.2.0 : mise en place de la gestion des items a partir de
 //                      fichiers de configuration (un fichier par menu)
-//    + Version 1.3.0 : prise en compte de l'etat initial (actif / inactif)
-//                      de chaque item de menu a sa creation
-//    + Version 1.4.0 : introduction de l'accesseur obtenirItem
-//    + Version 1.5.0 : introduction des methodes activerItem et desactiverItem
-//    + Version 1.6.0 : introduction de la methode ajouterEcouteur
-//    + Version 1.7.0 : introduction de la methode retirerEcouteur                   
+//    + Version 1.3.0 : introduction de tous les fichiers de configuration
+//                      des menus Word + Griser les items qui n'ont pas d'残outeur     
+//    + Version 1.4.0 : ajout d'un getter priver sur items      
+//    + Version 1.5.0 : ajout de methode de grisage ou de degrisage
+//						d'item.
+//    + Version 1.6.0 : ajout d'une methode d'ajout d'ecouteurs sur un item
+//    + Version 1.7.0 : ajout d'une methode de suppression d'ecouteurs sur un item
+//    + Version 1.8.0 : ajout d'une methode d'ajout de raccourci sur un item
 //
 // L'attribut "menus" de la classe MenusG est un dictionnaire dont les cles 
 // sont les noms des menus et dont les associes sont des references (au sens 
@@ -28,20 +30,25 @@
 // des references (au sens Java du terme) sur les instances de la classe 
 // MenuItem, ajoutees au menu cible.
 //
-// Auteur : A. Thuaire
+// Auteur : Hulin C仕ric
 //
-
-import java.util.*;
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.Frame;
+import java.awt.Menu;
+import java.awt.MenuBar;
+import java.awt.MenuItem;
+import java.awt.MenuShortcut;
+import java.awt.event.ActionListener;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 
 public class MenusG extends MenuBar {
+
 private LinkedHashMap menus;
 private LinkedHashMap items;
 private Frame hameconAmont;
 
-//                                                  *** Constructeur par defaut
-//
+   //                                             		*** Constructeur par defaut
+   //
    public MenusG (Frame parent) {                         
    	
       menus       = new LinkedHashMap();
@@ -49,8 +56,8 @@ private Frame hameconAmont;
       hameconAmont= parent;
    }
 
-//                                                  *** Constructeur normal
-//
+   //                                                  	*** Constructeur normal
+   //
    public MenusG (LinkedHashMap config, Frame parent) {    
    	
       this(parent);
@@ -87,7 +94,7 @@ private Frame hameconAmont;
       hameconAmont.setMenuBar(this);
    }
    
-   // --- Ajouter un nouveau menu avec ses items      *** Methode ajouterMenu
+   // Ajouter un nouveau menu avec ses items           	*** Methode ajouterMenu
    //
    public boolean ajouterMenu(String nomMenu, String associe) {
    LinkedHashMap config= null;
@@ -119,8 +126,7 @@ private Frame hameconAmont;
       
       // Charger le dictionnaire de configuration des items
       //
-      String cle= "Menu"+nomMenu;
-      config= (LinkedHashMap)Config.load(cle, associe);
+      config= (LinkedHashMap)Config.load(nomMenu, associe);
       if (config==null) return false;
       
       // Memoriser la configuration chargee dans le dictionnaire global de
@@ -130,64 +136,43 @@ private Frame hameconAmont;
       
       // Creer et ajouter tous les items au menu courant
       //
-      return ajouterItems(nomMenu);
+      return AjouterItems(nomMenu);
    }
    
-//  --- Module de tests unitaires                            *** Methode main
-//
+   //                                          	 		*** Methode main
+   //
    public static void main(String[] args) {
    LinkedHashMap config;
    
       // Creer un cadre par defaut
       //
       Frame f= new Frame();
-      f.setSize(700, 400);
+      f.setSize(900, 500);
       f.setTitle ("(AWT) Test de la barre de menus generique - V 1.7.0");
-      f.setLocation (400, 300);
+      f.setLocation (0, 0);
       
       // Charger la configuration de test de la barre de menus
       //
-      config= (LinkedHashMap)Config.load("ConfigMenusG", "1.7.0");
+      config= (LinkedHashMap)Config.load("ConfigMenusG", "1.2.0");
       
       // Creer et accrocher la barre de test a la fenetre support
       //
-      MenusG b= new MenusG(config, f);
+      MenusG menu= new MenusG(config, f);
+      
+      // Activer et Desactiver
+      //
+      menu.activerItem("Fichier", "Enregistrer");
 
       // Visualiser la fenetre de test
       //
       f.setVisible(true);
-      
-      // Ajouter dynamiquement un nouveau menu
-      //
-      b.ajouterMenu("Xxxxx", null);
-      
-      // Activer dynamiquement l'item "Enregister"
-      //
-      b.activerItem("Fichier", "Enregistrer");
-      
-      // Desactiver dynamiquement l'item "Nouveau"
-      //
-      b.desactiverItem("Fichier", "Nouveau");
-      
-      // Activer dynamiquement l'item "Enregister sous..."
-      //
-      b.activerItem("Fichier", "Enregistrer sous...");
-      
-      // Ajouter l'ecouteur de test a l'item "Enregistrer sous..."
-      //
-      b.ajouterEcouteur("Fichier", "Enregistrer sous...", 
-                        new EcouteurItemMenu("Enregistrer sous... !"));
-                        
-      // Retirer l'ecouteur de test a l'item "Enregistrer sous..."
-      //
-      b.retirerEcouteur("Fichier", "Enregistrer sous...", 1);
    }
    
-//  --- Creer et ajouter tous les items d'un menu     *** Methode ajouterItems
-//   
-   private boolean ajouterItems(String nomMenu) {
+   //  Creer et ajouter tous les items d'un menu    	*** Methode AjouterItems
+   //   
+   private boolean AjouterItems(String nomMenu) {
    LinkedHashMap config= null;
-   	
+    	
       // Controler la validite du parametre
       //
       if (nomMenu==null) return false;
@@ -202,186 +187,210 @@ private Frame hameconAmont;
       Iterator i=config.keySet().iterator();
       String cle=null;
       boolean ok= true;
-      Boolean associe= null;
    	     	
-      while(i.hasNext()) {
-
-         // Acquerir la cle courante (label de l'item)
+      while(i.hasNext()) 
+      {
+         // Acquerir la cle courante
          //
          cle=(String)i.next();
-         
-         // Acquerir l'associe (etat initial de l'item)
-         //
-         associe= (Boolean)config.get(cle);
-         
+
          // Creer et ajouter un nouvel item au menu cible
          // 
-         ok= ajouterItem(nomMenu, cle, associe);
+         ok= ajouterItem(nomMenu, cle);
          if(!ok) return false;
       }
-         
+
       return true;
    }
-   
-// --- Ajouter un nouvel item dans un menu cible      *** Methode ajouterItem
-//   
-   private boolean ajouterItem (String  nomMenu, 
-                                String  nomItem, 
-                                Boolean etatInitial) {
+         
+   // Ajouter un nouvel item dans un menu cible  		*** Methode ajouterItem
+   //   
+   private boolean ajouterItem(String nomMenu, String nomItem) {
    Menu menuCourant= null;
    MenuItem itemCourant= null;
-   LinkedHashMap w= null;
+   LinkedHashMap config= null;
    	
-      // Controler la validite des parametres
-      //
-      if (nomMenu==null) return false;
-      if (nomItem==null) return false;
+	   // Controler la validite des parametres
+	   //
+	   if (nomMenu==null) return false;
+	   if (nomItem==null) return false;
       
-      // Obtenir la reference du menu cible
-      //
-      menuCourant= (Menu)menus.get(nomMenu);
-      if (menuCourant==null) return false;
+	   // Obtenir la reference du menu cible
+	   //
+	   menuCourant= (Menu)menus.get(nomMenu);
+	   if (menuCourant==null) return false;
       
-      // Traiter le cas particulier d'une barre de separation
-      //
-      if (nomItem.substring(0,1).equals("-")){ 
-         menuCourant.addSeparator();
-         itemCourant= null;
-      }
-      else {
-   	  
-         // Creer et ajouter le nouvel item au menu cible
-         // 
-         itemCourant= new MenuItem(nomItem);
-         menuCourant.add(itemCourant);
+	   // Traiter le cas particulier d'une barre de separation
+	   //
+	   if (nomItem.substring(0,1).equals("-"))
+	   { 
+		   menuCourant.addSeparator();
+		   itemCourant= null;
+	   }
+	   else 
+	   {
+		   // Creer et ajouter le nouvel item au menu cible
+		   // 
+		   itemCourant= new MenuItem(nomItem);
+		   
+		   if(((LinkedHashMap) items.get(nomMenu)).get(nomItem) == null)	itemCourant.setEnabled(false);
          
-         // Traiter l'etat initial de l'item
-         //
-         if (etatInitial != null && 
-             ! etatInitial.booleanValue())itemCourant.setEnabled(false);
-      }
+		   menuCourant.add(itemCourant);
+	   }
       
-      // Ajouter la reference du nouvel item dans le dictionnaire de tous les
-      // items (attribut)
-      //
-      w= (LinkedHashMap)items.get(nomMenu);
-      if (w==null) return false;
+	   // Ajouter la reference du nouvel item dans le dictionnaire de tous les
+	   // items (attribut)
+	   //
+	   config= (LinkedHashMap)items.get(nomMenu);
+	   if (config==null) return false;
       
-      w.put(nomItem, itemCourant);
+	   config.put(nomItem, itemCourant);
+	   items.put(nomMenu, config);
    
-      return true;
+	   return true;
    }
    
-// --- Obtenir la reference d'un item de menu         *** Methode obtenirItem
-//   
-   public MenuItem obtenirItem (String nomMenu, String  nomItem) {
-   	
-      // Controler la validite des parametres
-      //
-      if (nomMenu==null) return null;
-      if (nomItem==null) return null;
-      
-      // Obtenir le dictionnaire des items du menu cible
-      //
-      LinkedHashMap w= (LinkedHashMap)items.get(nomMenu);
-      if (w==null) return null;
-      
-      // Obtenir la reference de l'item cible
-      //
-      return (MenuItem)w.get(nomItem);
-   }
-   
-// --- Activer un item d'un menu cible               *** Methode activerItem
-//   
-   public boolean activerItem (String  nomMenu, String  nomItem) {
-        
-      // Obtenir la reference de l'item cible
-      //
-      MenuItem itemCible= obtenirItem(nomMenu, nomItem);
-      if (itemCible==null) return false;
-      
-      // Activer l'item cible
-      //
-      itemCible.setEnabled(true);
-      return true;
-   }
-   
-// --- Desactiver un item d'un menu cible            *** Methode desactiverItem
-//   
-   public boolean desactiverItem (String  nomMenu, String  nomItem) {
-   	
-      // Obtenir la reference de l'item cible
-      //
-      MenuItem itemCible= obtenirItem(nomMenu, nomItem);
-      if (itemCible==null) return false;
-      
-      // Desactiver l'item cible
-      //
-      itemCible.setEnabled(false);
-      return true;
-   }
-   
-// --- Attacher un ecouteur a un item cible        *** Methode ajouterEcouteur
-//   
-   public boolean ajouterEcouteur (String         nomMenu, 
-                                   String         nomItem,
-                                   ActionListener ecouteur) {
-        
-      // Obtenir la reference de l'item cible
-      //
-      MenuItem itemCible= obtenirItem(nomMenu, nomItem);
-      if (itemCible==null) return false;
-      
-      // Controler la validite du troisieme parametre
-      //
-      if(ecouteur == null) return false;
-      
-      // Ajouter l'ecouteur fourni en parametre
-      //
-      itemCible.addActionListener(ecouteur);
-      return true;
-   }
-   
-// --- Retirer un ecouteur a un item cible         *** Methode retirerEcouteur
-//   
-   public boolean retirerEcouteur (String nomMenu, String nomItem,int rang) {
-   	
-   	  // Obtenir la reference de l'item cible
-      //
-      MenuItem itemCible= obtenirItem(nomMenu, nomItem);
-      if (itemCible==null) return false;
-      
-      // Obtenir le tableau courant des ecouteurs attaches a l'item
-      //
-      ActionListener[] ecouteurs= itemCible.getActionListeners();
-      if (ecouteurs.length == 0) return false;
-  
-      // Controler la validite du premier parametre
-      //
-      if(rang <= 0 || rang > ecouteurs.length) return false;
-      
-      // Retirer l'ecouteur designe par son rang
-      //
-      itemCible.removeActionListener(ecouteurs[rang-1]);
-      
-      // Desactiver l'item cible si opportun 
-      //
-      if (ecouteurs.length - 1 == 0) itemCible.setEnabled(false);
-      return true;
-   }
-   
-   // --- Ecouteur de tests des items de menus
+   // Obtenir un item									*** Methode obtenirItem
    //
-   private static class EcouteurItemMenu implements ActionListener {
-   private String labelItem;
+   private MenuItem obtenirItem(String nomMenu, String nomItem) {
+   MenuItem itemCourant= null;
+		   	
+	   // Controler la validite des parametres
+	   //
+	   if (nomMenu==null) return null;
+	   if (nomItem==null) return null;
+		      
+	   // Obtenir la reference du MenuItem cible
+	   //
+	   itemCourant= (MenuItem) ((LinkedHashMap) items.get(nomMenu)).get(nomItem);
+	   if(itemCourant == null)	return null;
+		     
+	   // Retourner la reference sur le MenuItem
+	   //
+	   return itemCourant;
+   }
+   
+   // Griser un item									*** Methode activerItem
+   //
+   public boolean activerItem(String nomMenu, String nomItem) {
+   MenuItem itemCourant= null;
+	   	
+	   // Controler la validite des parametres
+	   //
+	   if (nomMenu==null) return false;
+	   if (nomItem==null) return false;
+		      
+	   // Obtenir la reference du MenuItem cible
+	   //
+	   itemCourant= obtenirItem(nomMenu, nomItem);
+	   if (itemCourant == null)	return false;
+		     
+	   // Degriser le MenuItem
+	   //
+	   itemCourant.setEnabled(true);
+	   return true;
+   }
+   
+   // Degriser un item									*** Methode desactiverItem
+   //
+   public boolean desactiverItem(String nomMenu, String nomItem) {
+   MenuItem itemCourant= null;
+	   	
+	   // Controler la validite des parametres
+	   //
+	   if (nomMenu==null) return false;
+	   if (nomItem==null) return false;
+		      
+	   // Obtenir la reference du MenuItem cible
+	   //
+	   itemCourant= obtenirItem(nomMenu, nomItem);
+	   if (itemCourant == null)	return false;
+		     
+	   // Griser le MenuItem
+	   //
+	   itemCourant.setEnabled(false);
+	   return true;
+   }
+   
+   // Ajouter un ecouteur a un item						*** Methode ajouterEcouteur
+   //
+   public boolean ajouterEcouteur(String nomMenu , String nomItem, ActionListener ecouteur) {
+   MenuItem itemCourant=null; 
    	
-      public EcouteurItemMenu(String label) {
-         labelItem= label;
-      }
-   	
-      public void actionPerformed (ActionEvent e) {
-         System.out.println(labelItem);
-      }
-   }         
-}
+	   // Controler la validite des parametres
+	   //
+	   if (nomMenu==null) return false;
+	   if (nomItem==null) return false;
+	   
+	   // Obtenir la reference du MenuItem cible
+	   //
+	   itemCourant= obtenirItem(nomMenu, nomItem);
+	   if (itemCourant == null)	return false;
+ 
+	   // D使riser l'item
+	   //
+	   if (!activerItem(nomMenu, nomItem)) return false;
+      
+	   // Ajouter l'残outeur sur l'item
+	   //
+	   itemCourant.addActionListener(ecouteur);
+	   return true;
+   	}
+   
+   // Enlever un ecouteur a un item						*** Methode enleverEcouteur
+   //
+   public boolean enleverEcouteur(String nomMenu, String nomItem, int rang) {
+   MenuItem itemCourant= null;
+   ActionListener ecouteur= null;
 
+	   // Controler la validite des parametres
+	   //
+	   if (nomMenu==null) return false;
+	   if (nomItem==null) return false;
+      
+	   // Obtenir la reference du MenuItem cible
+	   //
+	   itemCourant= obtenirItem(nomMenu, nomItem);
+	   if (itemCourant == null)	return false;
+      	      
+	   // Verifier l'existence de l'ecouteur
+	   // 
+	   if (itemCourant.getActionListeners().length < rang)	return false;
+	   ecouteur= itemCourant.getActionListeners()[rang];
+	   if (ecouteur == null)	return false;
+	   
+	   // Enlever l'ecouteur sur l'item
+	   //
+	   itemCourant.removeActionListener(ecouteur);
+      
+	   // Griser l'item s'il n'a plus d'残outeurs
+	   //
+	   if(itemCourant.getActionListeners().length == 0)	itemCourant.setEnabled(false);
+	   return true;
+   }
+   
+   // Ajouter un raccourci a un item					*** Methode ajouterRaccourci
+   //  
+   public boolean ajouterRaccourci(String nomMenu, String nomItem, MenuShortcut  raccourci) {
+   MenuItem itemCourant= null;
+	   	
+	   // Controler la validite des parametres
+	   //
+	   if (nomMenu==null) return false;
+	   if (nomItem==null) return false;
+	      
+	   // Obtenir la reference du MenuItem cible
+	   //
+	   itemCourant= obtenirItem(nomMenu, nomItem);
+	   if (itemCourant == null)	return false;
+	     
+	   // Attacher un raccourci a l'item courant
+	   //
+	   itemCourant.setShortcut(raccourci);
+	      
+	   // D使riser l'item
+	   //
+	   itemCourant.setEnabled(true);
+	   return true;
+   }
+}
